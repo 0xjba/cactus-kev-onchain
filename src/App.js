f7cb1b6c36ad5789ab45a5ca7d9d4e10ce38c6bf
Jobin Ayathil <jobinb6444@outlook.com>
2024-12-06 18:20:09 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PokerEvaluator from './components/PokerEvaluator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PokerEvaluator />
+    </div>
+  );
+}
+
+export default App;
